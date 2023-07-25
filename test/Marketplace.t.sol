8996fc2b082c248e4dc590505b77bae988819338
Dennoh Peter <mail@dennohpeter.com>
2023-07-25 21:08:16 +0300

    :gear: project setup update

---
+++ test/Marketplace.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import {Marketplace} from "@contracts/Marketplace.sol";
+
+contract MarketplaceTest is Test {
+    Marketplace public marketplace;
+
+    function setUp() public {}
+}
