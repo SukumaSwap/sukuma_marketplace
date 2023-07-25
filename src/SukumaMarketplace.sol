e7315596b97ed9bcb75c2d9c285509224fbef574
timothyAgevi <timothyagevi@gmail.com>
2023-07-25 13:37:23 +0300

    restructure events,variables and functions

--- src/SukumaMarketplace.sol
+++ src/SukumaMarketplace.sol
@@ -1,20 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-    //necessary imports not yet installed
+//necessary imports not yet installed
 import "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 // Importing OpenZeppelin's ERC20 interface
 import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
 
 import "openzeppelin/token/ERC20/ERC20.sol";
+
 // import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract SukumaMarketplace is Initializable, OwnableUpgradeable{
-   //enums
-    enum OfferType {Buy, Sell}
-    enum OfferStatus {Open, Closed}
-    enum TradeType {Buy, Sell}
-    enum TradeStatus {Active, Disputed, Completed}
- // Structs
+contract SukumaMarketplace is Initializable, OwnableUpgradeable {
+    //enums
+    enum OfferType {
+        Buy,
+        Sell
+    }
+    enum OfferStatus {
+        Open,
+        Closed
+    }
+    enum TradeType {
+        Buy,
+        Sell
+    }
+    enum TradeStatus {
+        Active,
+        Disputed,
+        Completed
+    }
+    // Structs
     struct Offer {
         uint256 offerId;
         address token;
@@ -60,57 +74,95 @@ contract SukumaMarketplace is Initializable, OwnableUpgradeable{
 
     // Variables
     uint256 private marketplaceFee;
+    //variable to count trades
+    uint256 public tradeCounter = 0;
+    // Variable to keep track of offerId
+    uint256 public offerIdCounter;
+
     // address public owner;
     mapping(uint256 => Offer) public offers;
     mapping(uint256 => Trade) public trades;
     mapping(uint256 => Transfer) public transfers;
     // mapping(uint256 => Account) public accounts; //to be deleted
 
-     // Mapping of wallet address to Account
+    // Mapping of wallet address to Account
     mapping(address => Account) private accounts;
     uint64 private nextAccountId = 1; // Account ID starts at 1
     // Mapping of accountId to Eth address
-mapping(uint64 => address) private idToAddress;
+    mapping(uint64 => address) private idToAddress;
 
     // Events
     event OfferCreated(uint256 offerId);
     event TradeCreated(uint256 tradeId);
     event TransferCreated(uint256 transferId);
     event AccountCreated(address walletAddress, uint256 accountId);
+    // Event to be emitted when an offer is closed
+    event OfferClosed(uint256 offerId);
+    // Define an event
+    event CryptoReleased(
+        uint256 indexed tradeId,
+        address token,
+        uint256 quantity,
+        address receiver
+    );
+    // Event to emit when the marketplace fee is changed
+    event MarketplaceFeeChanged(uint256 newFee);
+     // This event will be emitted when a user withdraws tokens
+    event Withdrawal(
+        address indexed user,
+        address indexed token,
+        uint256 amount
+    );
 
-     // Initializer - replaces the constructor when using the upgradeable pattern
-       function initialize() public initializer {
+    // Initializer - replaces the constructor when using the upgradeable pattern
+    function initialize() public initializer {
         __Ownable_init();
     }
 
-
     // Modifier
-   
 
     // Functions
     function createAccount() public returns (uint256) {
-         // Ensure account does not already exist
-        require(accounts[msg.sender].walletAddress == address(0), "Account already exists");
+        // Ensure account does not already exist
+        require(
+            accounts[msg.sender].walletAddress == address(0),
+            "Account already exists"
+        );
         // Create new account
         Account storage account = accounts[msg.sender];
         account.walletAddress = msg.sender;
         account.accountId = nextAccountId;
-         // Emit event
+        // Emit event
         emit AccountCreated(msg.sender, nextAccountId);
-         // Increment next account ID
-        nextAccountId = nextAccountId+ 1;
-         return account.accountId;
+        // Increment next account ID
+        nextAccountId = nextAccountId + 1;
+        return account.accountId;
     }
-  // Function to get account information
-    function getAccount(address walletAddress) public view returns (uint256 accountId, uint256 likes, uint256 dislikes, uint256 Blocks) {
-        Account storage account = accounts[walletAddress];
-        return (account.accountId, account.likes, account.dislikes, account.Blocks);
-    }
-    
-    // Variable to keep track of offerId
-    uint256 public offerIdCounter;
 
-    function createOffer(address _token,
+    // Function to get account information
+    function getAccount(
+        address walletAddress
+    )
+        public
+        view
+        returns (
+            uint256 accountId,
+            uint256 likes,
+            uint256 dislikes,
+            uint256 Blocks
+        )
+    {
+        Account storage account = accounts[walletAddress];
+        return (
+            account.accountId,
+            account.likes,
+            account.dislikes,
+            account.Blocks
+        );
+    }
+
+    function createOffer(
+        address _token,
         uint256 _quantity,
         OfferType _offerType,
         uint256 _min,
@@ -120,13 +172,13 @@ mapping(uint64 => address) private idToAddress;
         string[] memory _acceptedCurrency,
         string[] memory _paymentMethods,
         OfferStatus _offerStatus
-        ) public   {
-         require(_token != address(0), "_token address cannot be zero");
+    ) public {
+        require(_token != address(0), "_token address cannot be zero");
         require(_quantity > 0, "_quantity must be greater than zero");
-         // incrementing the offerIdCounter for each new offer
-        offerIdCounter++ ;
+        // incrementing the offerIdCounter for each new offer
+        offerIdCounter++;
 
-         Offer memory newOffer = Offer({
+        Offer memory newOffer = Offer({
             offerId: offerIdCounter,
             token: _token,
             quantity: _quantity,
@@ -139,33 +191,31 @@ mapping(uint64 => address) private idToAddress;
             paymentMethods: _paymentMethods,
             offerStatus: _offerStatus
         });
-// Saving the offer in offers mapping
+        // Saving the offer in offers mapping
         offers[offerIdCounter] = newOffer;
     }
-//variable to count trades
-  uint256 public tradeCounter = 0;
 
-    function createBuyTrade( 
+    function createBuyTrade(
         uint256 orderId,
-       
         uint256 quantity,
         address receiver,
         address sender,
         address token,
         TradeType tradeType,
-        uint64 amount) public {
-            //input validators
-     require(token != address(0), "token address cannot be zero");
-     require(quantity > 0, "quantity must be greater than zero");  
-    // Require that the tradingType is Buy
+        uint64 amount
+    ) public {
+        //input validators
+        require(token != address(0), "token address cannot be zero");
+        require(quantity > 0, "quantity must be greater than zero");
+        // Require that the tradingType is Buy
         require(tradeType == TradeType.Buy, "TradeType must be Buy");
-// Require that the offerId exists
+        // Require that the offerId exists
         require(offers[orderId].offerId == orderId, "OfferId does not exist");
-      
-       // Autogenerate tradeId
+
+        // Autogenerate tradeId
         tradeCounter++;
-        uint256 tradeId = tradeCounter; 
-// Create a new trade
+        uint256 tradeId = tradeCounter;
+        // Create a new trade
         Trade memory trade = Trade({
             tradeId: tradeId,
             orderId: orderId,
@@ -175,30 +225,31 @@ mapping(uint64 => address) private idToAddress;
             sender: sender,
             token: token,
             tradeType: TradeType.Buy,
-            amount:amount
+            amount: amount
         });
-         // Store the trade
+        // Store the trade
         trades[tradeId] = trade;
     }
 
-    function createSellTrade(uint256 orderId,
-       
+    function createSellTrade(
+        uint256 orderId,
         uint256 quantity,
         address receiver,
         address sender,
         address token,
         TradeType tradeType,
-        uint64 amount) public {
+        uint64 amount
+    ) public {
         require(token != address(0), "token address cannot be zero");
         require(quantity > 0, "quantity must be greater than zero");
-    // Require that the tradingType is Buy
+        // Require that the tradingType is Buy
         require(tradeType == TradeType.Sell, "TradeType must be Sell");
-// Require that the offerId exists
+        // Require that the offerId exists
         require(offers[orderId].offerId == orderId, "OfferId does not exist");
-       // Autogenerate tradeId
+        // Autogenerate tradeId
         tradeCounter++;
-        uint256 tradeId = tradeCounter; 
-// Create a new trade
+        uint256 tradeId = tradeCounter;
+        // Create a new trade
         Trade memory trade = Trade({
             tradeId: tradeId,
             orderId: orderId,
@@ -208,20 +259,18 @@ mapping(uint64 => address) private idToAddress;
             sender: sender,
             token: token,
             tradeType: TradeType.Sell,
-            amount:amount
+            amount: amount
         });
-         // Store the trade
+        // Store the trade
         trades[tradeId] = trade;
     }
 
- // Event to emit when the marketplace fee is changed
-    event MarketplaceFeeChanged(uint256 newFee);
-// Function to get the current marketplace fee
+    // Function to get the current marketplace fee
     function getMarketplaceFee() public view returns (uint256) {
         return marketplaceFee;
     }
 
-     // Function to set the marketplace fee, can only be called by the contract owner
+    // Function to set the marketplace fee, can only be called by the contract owner
     function setMarketplaceFee(uint256 _fee) public onlyOwner {
         marketplaceFee = _fee;
 
@@ -234,24 +283,22 @@ mapping(uint64 => address) private idToAddress;
         IERC20 token = IERC20(_token);
         // Transfer the tokens to this contract
         token.transferFrom(msg.sender, address(this), _amount);
-         // Update the account's balance
+        // Update the account's balance
         accounts[msg.sender].Balance[_token] += _amount;
-
     }
 
-// This event will be emitted when a user withdraws tokens
-    event Withdrawal(address indexed user, address indexed token, uint256 amount);
 
-
-   function checkBalance(address _account, address _token) external view returns (uint256) {
+    function checkBalance(
+        address _account,
+        address _token
+    ) external view returns (uint256) {
         // Return the balance of the account
-         return accounts[_account].Balance[_token];
+        return accounts[_account].Balance[_token];
     }
 
-
-    function transfer(address _token, uint256 _quantity, address _to) public { 
+    function transfer(address _token, uint256 _quantity, address _to) public {
         require(_quantity > 0, "Transfer quantity must be greater than zero");
-         require(_to != address(0), "Receiver address cannot be zero address");
+        require(_to != address(0), "Receiver address cannot be zero address");
 
         // IERC20(_token) allows the contract to interact with the ERC20 token at address _token
         IERC20 token = IERC20(_token);
@@ -261,67 +308,73 @@ mapping(uint64 => address) private idToAddress;
         token.transfer(_to, _quantity);
     }
 
-// Define an event
-event CryptoReleased(uint256 indexed tradeId, address token, uint256 quantity, address receiver);
+    function releaseCrypto(
+        address _token,
+        uint256 _quantity,
+        uint256 _tradeId,
+        address _receiver,
+        uint256 _balance
+    ) public {
+        require(_balance >= _quantity, "Insufficient balance");
 
-  function releaseCrypto(address _token, uint256 _quantity, uint256 _tradeId, address _receiver, uint256 _balance) public {
-    require(_balance >= _quantity, "Insufficient balance");
+        // IERC20(_token) allows the contract to interact with the ERC20 token at address _token
+        IERC20 token = IERC20(_token);
 
-    // IERC20(_token) allows the contract to interact with the ERC20 token at address _token
-    IERC20 token = IERC20(_token);
+        // Transfer the tokens
+        require(token.transfer(_receiver, _quantity), "Token transfer failed");
 
-    // Transfer the tokens
-    require(token.transfer(_receiver, _quantity), "Token transfer failed");
+        // Update the balance
+        _balance -= _quantity;
 
-    // Update the balance
-    _balance -= _quantity;
-
-    // Emit an event for the token release
-    emit CryptoReleased(_tradeId, _token, _quantity, _receiver);
-}
-// Event to be emitted when an offer is closed
-    event OfferClosed(uint256 offerId);
+        // Emit an event for the token release
+        emit CryptoReleased(_tradeId, _token, _quantity, _receiver);
+    }
 
     function closeOffer(uint256 _offerId) public {
-     // Checking if the offer exists
+        // Checking if the offer exists
         require(offers[_offerId].offerId == _offerId, "Offer does not exist");
-          // Checking if the offer is not already closed
-        require(offers[_offerId].offerStatus != OfferStatus.Closed, "Offer is already closed");
+        // Checking if the offer is not already closed
+        require(
+            offers[_offerId].offerStatus != OfferStatus.Closed,
+            "Offer is already closed"
+        );
         // Closing the offer
         offers[_offerId].offerStatus = OfferStatus.Closed;
         // Emitting the OfferClosed event
         emit OfferClosed(_offerId);
     }
-function like(uint64 _accountId) public {
-    // Retrieve the wallet address associated with the account ID
-    address accountAddress = idToAddress[_accountId];
 
-    // Ensure the account exists
-    require(accountAddress != address(0), "Account does not exist");
+    function like(uint64 _accountId) public {
+        // Retrieve the wallet address associated with the account ID
+        address accountAddress = idToAddress[_accountId];
 
-    // Increment the likes count for the account
-    accounts[accountAddress].likes += 1;
-}
-       function dislike(uint64 _accountId) public {
-         // Retrieve the wallet address associated with the account ID
-    address accountAddress = idToAddress[_accountId];
+        // Ensure the account exists
+        require(accountAddress != address(0), "Account does not exist");
 
-    // Ensure the account exists
-    require(accountAddress != address(0), "Account does not exist");
+        // Increment the likes count for the account
+        accounts[accountAddress].likes += 1;
+    }
 
-    // Increment the likes count for the account
-    accounts[accountAddress].dislikes += 1;
+    function dislike(uint64 _accountId) public {
+        // Retrieve the wallet address associated with the account ID
+        address accountAddress = idToAddress[_accountId];
+
+        // Ensure the account exists
+        require(accountAddress != address(0), "Account does not exist");
+
+        // Increment the likes count for the account
+        accounts[accountAddress].dislikes += 1;
     }
 
     function blockAccount(uint64 _accountId) public {
-              // Retrieve the wallet address associated with the account ID
-    address accountAddress = idToAddress[_accountId];
+        // Retrieve the wallet address associated with the account ID
+        address accountAddress = idToAddress[_accountId];
 
-    // Ensure the account exists
-    require(accountAddress != address(0), "Account does not exist");
+        // Ensure the account exists
+        require(accountAddress != address(0), "Account does not exist");
 
-    // Increment the likes count for the account
-    accounts[accountAddress].Blocks += 1;
+        // Increment the likes count for the account
+        accounts[accountAddress].Blocks += 1;
     }
 }
 //Recommendations:
